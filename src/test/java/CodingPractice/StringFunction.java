254484dfa94f63cd8bb2f6e68c8e911c400c631d
Md Ashraf Siddique <connect.ashraf7464@gmail.com>
2024-01-25 22:22:39 -0500

    Jan 25

--- src/test/java/CodingPractice/StringFunction.java
+++ src/test/java/CodingPractice/StringFunction.java
@@ -10,7 +10,7 @@ public class StringFunction {
 
 
     @Test
-    public void lengthOfString(){
+    public void lengthOfString() {
         String name = "Md Ashraf Siddique";
         int length = name.length();
         System.out.println(length);
@@ -18,16 +18,16 @@ public class StringFunction {
     }
 
     @Test
-    public void numberOfChar(){
+    public void numberOfChar() {
         String name = "Md Ashraf Siddique";
-        String newName =name.replace(" ", "");
+        String newName = name.replace(" ", "");
         System.out.println(newName.length());
     }
 
     @Test
-    public void charOccurrences(){
+    public void charOccurrences() {
         String name = "Md Ashraf Siddique";
-        String name1 = name.replace(" ","").toLowerCase();
+        String name1 = name.replace(" ", "").toLowerCase();
         for (char c : name1.toCharArray()) {
             int count = 0;
             for (char ch : name1.toCharArray()) {
@@ -35,7 +35,7 @@ public class StringFunction {
                     count++;
                 }
             }
-            System.out.println("Characters "+c+" appears "+count+" times.");
+            System.out.println("Characters " + c + " appears " + count + " times.");
         }
 
     }
@@ -46,124 +46,125 @@ public class StringFunction {
         System.out.println(name.toUpperCase());
         System.out.println(name.toLowerCase());
     }
+
     @Test
-    public void splitString(){
+    public void splitString() {
         String name = "Md Ashraf Siddique";
-        String [] words = name.split(" ");
+        String[] words = name.split(" ");
         System.out.println(Arrays.toString(words));
         System.out.println(words[1]);
     }
 
     @Test
-    public void StringToInteger(){
+    public void StringToInteger() {
         String number = "100";
         int number1 = Integer.parseInt(number);
 
-        System.out.println(100+number);
-        System.out.println(100+number1);
+        System.out.println(100 + number);
+        System.out.println(100 + number1);
     }
 
     @Test
-    public void IntegerToString(){
+    public void IntegerToString() {
         int number = 100;
         String number1 = String.valueOf(number);
-        System.out.println(100+number);
-        System.out.println(100+number1);
+        System.out.println(100 + number);
+        System.out.println(100 + number1);
     }
 
     @Test
-    public void getIntegerValueFromString(){
+    public void getIntegerValueFromString() {
         String str = "MD87ASH56RAF123";
         int number = Integer.parseInt(str);
         System.out.println(number);
     }
 
     @Test
-    public void reverseString(){
+    public void reverseString() {
         String name = "Md Ashraf Siddique";
-        for(int i=name.length()-1; i>=0; i--){
+        for (int i = name.length() - 1; i >= 0; i--) {
             System.out.print(name.charAt(i));
         }
     }
 
     @Test
-    public void reverseAtWords(){
-        String myName ="My Name Is Ashraf";
-        String [] words=myName.split(" ");
-        String reverseWords="";
-        for(int i=words.length-1; i>=0; i--){
-            reverseWords = reverseWords+words[i];
-            if(i!=0){
-                reverseWords=reverseWords+" ";
+    public void reverseAtWords() {
+        String myName = "My Name Is Ashraf";
+        String[] words = myName.split(" ");
+        String reverseWords = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            reverseWords = reverseWords + words[i];
+            if (i != 0) {
+                reverseWords = reverseWords + " ";
             }
         }
         System.out.println(reverseWords);
     }
 
     @Test
-    public void subString(){
-        String name="Md Ashraf Siddique";
-        String newName=name.substring(0,9);
+    public void subString() {
+        String name = "Md Ashraf Siddique";
+        String newName = name.substring(0, 9);
         System.out.println(newName);
     }
+
     @Test
-    public void concatenation(){
-        String str1 ="Md Ashraf";
-        String str2=" Siddique";
+    public void concatenation() {
+        String str1 = "Md Ashraf";
+        String str2 = " Siddique";
         System.out.println(str1.concat(str2));
     }
+
     @Test
-    public void replace(){
-        String email="ashraf#gmail.com";
-        String email1=email.replace("#","@");
+    public void replace() {
+        String email = "ashraf#gmail.com";
+        String email1 = email.replace("#", "@");
         System.out.println(email1);
 
-        String email2 = email1.replace("gmail","mail");
+        String email2 = email1.replace("gmail", "mail");
         System.out.println(email2);
     }
+
     @Test
-    public void stringContainsChar(){
+    public void stringContainsChar() {
         String str = "MD8@7ASH56RAF123";
-        if(str.contains("@")){
+        if (str.contains("@")) {
             System.out.println("String has @");
-        }
-        else{
+        } else {
             System.out.println("String has no @");
         }
     }
+
     @Test
-    public void startsAndEndsWith(){
-        String str="Today is Friday, Jan 15, 2024";
-        if(str.startsWith("Today")){
+    public void startsAndEndsWith() {
+        String str = "Today is Friday, Jan 15, 2024";
+        if (str.startsWith("Today")) {
             System.out.println("Str starts with Today");
-        }
-        else{
+        } else {
             System.out.println("Wrong");
         }
         System.out.println("*************");
-        if(str.endsWith("2024")){
+        if (str.endsWith("2024")) {
             System.out.println("Str ends with 2024");
-        }
-        else{
+        } else {
             System.out.println("Wrong");
         }
     }
+
     @Test
-    public void hasVowels(){
+    public void hasVowels() {
         String country = "Bangladesh";
         String str = country.toUpperCase();
-        for(int i=0; i<str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if(c=='A' || c=='E' || c=='I' ||c=='O'||c=='U'){
-                System.out.println(c+" is a vowels");
-            }
-            else{
-                System.out.println(c+" is not a vowels");
+            if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                System.out.println(c + " is a vowels");
+            } else {
+                System.out.println(c + " is not a vowels");
             }
 
         }
     }
-
 
 
 }
